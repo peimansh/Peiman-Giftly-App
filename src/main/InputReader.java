fc706ba4bf601a3e0106902f20cf338defd0a1ea
torinorgist <32059257+torinorgist@users.noreply.github.com>
2017-11-09 17:17:12 +0100

    added method to the main so the app can start and run.

--- src/main/InputReader.java
+++ src/main/InputReader.java
@@ -14,7 +14,7 @@ public class InputReader {
 		System.out.println("This App let's you add your loved ones as contacts to the app and add ");
 		System.out.println("the items they like to have as wish list and add some important");
 		System.out.println("dates like their birthday, anniversaries etc.. and receive suggestions ");
-		System.out.println("so at the events you will receive a random suggestion to buy as a gift.");
+		System.out.println("so at the events you will receive a random suggestion to buy something as a gift.");
 		System.out.println("Use these numbers to begin :");
 		System.out.println("1- Add a contact");
 		System.out.println("2- Delete a contact");
