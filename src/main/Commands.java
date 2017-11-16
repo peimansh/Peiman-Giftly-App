42bb3c5d04bad1536273e4bf0ea5d611e5d4731d
Unknown <peiman.shokouhian@gmail.com>
2017-11-16 09:02:08 +0100

    Tons of improvements
    But the app is not running !

--- src/main/Commands.java
+++ src/main/Commands.java
@@ -12,13 +12,17 @@ public class Commands {
 	private Scanner reader;
 	private Contact contact;
 	private ArrayList<Contact> contacts;
+	private int readerInt;
+	private int input;
 
 	public Commands() {
 		contacts = new ArrayList<>();
     	reader = new Scanner(System.in);
+    	readerInt = Integer.parseInt(reader.nextLine());
+    	input = (reader.nextInt());
 	}
-	public void welcomeMsg() throws IOException {
-		
+	
+	public void welcomeMsg() throws IOException {	
 		System.out.println("Welcome to the Gift Management app \"Giftly\" .");
 		System.out.println("This App let's you add your loved ones as contacts to the application and add ");
 		System.out.println("the items they like to have as wish list and add some important");
@@ -26,34 +30,33 @@ public class Commands {
 		System.out.println("so at the events you will receive a random suggestion to buy something as a gift.");
 		anotherWelcomeMsg();
 	}
-	public void anotherWelcomeMsg() throws IOException{
-		
+	
+	public void anotherWelcomeMsg() throws IOException{	
 		boolean b = true;
 		System.out.println("Use these numbers to begin :");
 		System.out.println("1- Add a contact");
 		System.out.println("2- Delete a contact");
-		System.out.println("3- Show list of contacts");
-		System.out.println("4- Save the info to a file");
-		System.out.println("5- Load the info from file");
-		System.out.println("6- Quit");
+		System.out.println("3- Modify a contact");
+		System.out.println("4- Show list of contacts");
+		System.out.println("5- Save the info to a file");
+		System.out.println("6- Load the info from file");
+		System.out.println("7- Quit");
 		while (b) {
 			try {
-
-				int n = Integer.parseInt(reader.nextLine());
-				if (n>0 && n<7) {
+				if (readerInt>0 && readerInt<8) {
 					b = false;
-					if(n==1){
+					if(readerInt==1){
 						addContactMsg() ;
 					}
-					else if(n==2){
+					else if(readerInt==2){
 						deleteContactMsg();
 					}
-					else if(n==3){
+					else if(readerInt==3){
 
-						;
+						
 					}
-					else if(n==4){
-						;
+					else if(readerInt==4){
+						
 					}
 				}
 				} catch (NumberFormatException e) {
@@ -71,21 +74,20 @@ public class Commands {
 		contacts.add(contact);
 		boolean a = true;
 		while (a) {
-			System.out.println("1-Add an interest for the person\n2-Add an event for the person");
-			try {
-				int n = Integer.parseInt(reader.nextLine());
-                if (n>0&&n<3) {
+				System.out.println("1-Add an interest for the person\n2-Add an event for the person");
+				try {
+					if (readerInt>0 && readerInt<3) {
                     a = true;
-                    if(n==1){
-                    	System.out.println("Enter the description of the interest item");
+                    if (readerInt==1) {
+                    	System.out.println("Enter the description of the interest item:");
                     	String wishname  = reader.nextLine();
-                    	System.out.println("Enter the estimated price of the item");
+                    	System.out.println("Enter the estimated price of the item:");
                     	String price = reader.nextLine();
                     	int wishprice = Integer.parseInt(price);
                     	contact.addWish(wishname, wishprice);
                     	System.out.println("Interest successfully added for the contact");
                     	System.out.println("Do you want to add another Interest or Event for the contact? Y/N");
-                    	String s = reader.nextLine().toLowerCase();
+                    	String s = reader.nextLine().trim().toLowerCase();
                     		if (s.equals("y")) {
                     			a = true;
                     			continue;
@@ -96,7 +98,7 @@ public class Commands {
                     	}
                     	}
                     	}
-                	if(n==2){
+                	if(readerInt==2){
                     	System.out.println("Enter the description of the event");
                     	String description = reader.nextLine();
                     	System.out.println("Enter the date of the event");
@@ -109,46 +111,50 @@ public class Commands {
                         	} catch (ParseException e) {
                             e.printStackTrace();
                         	}
-                    		System.out.println("Enter the address of the event");
-                    		String address = reader.nextLine();
-                    		contact.addEvent(description, date, address);
-                    		System.out.println("Event successfully added for the contact");
-                    		System.out.println("Do you want to add another Interest or Event for the contact? Y/N");
-                    		String s = reader.nextLine().toLowerCase();
+                    	System.out.println("Enter the address of the event");
+                    	String address = reader.nextLine();
+                    	contact.addEvent(description, date, address);
+                    	System.out.println("Event successfully added for the contact");
+                    	System.out.println("Do you want to add another Interest or Event for the contact? Y/N");
+                    	String s = reader.nextLine().trim().toLowerCase();
                     		if (s.equals("y")) {
                     			a = true;
+                    			continue;
                     		}else{
                     			a = false;
                     			printL();
                     			anotherWelcomeMsg();
                     		}
-                    	}
-                    	else {
-                    	System.out.println(n + " Isn't a valid option... please enter a number between 1 and 2");
-                    	}    
-                    }
+                	}
+				}	
+                    
 			
 					catch (NumberFormatException e) {
-						System.out.println("Enter a number between 1 to 2 please...");   
+						System.out.println(readerInt+"isn't a valid option. Please enter a number between 1 and 2 ...");   
 					}
 			}
 		}
 	
 	public void deleteContactMsg() throws IOException {
-		if (contacts.size()==0) {
+			if (contacts.size()==0) {
 			System.out.println("There is no contact to delete !");
 			anotherWelcomeMsg();
-		}
-		System.out.println("Write the number of the contact you want to delete:");
-		for (int i=0; i<contacts.size(); i++) {
+			}
+			System.out.println("Write the number of the contact you want to delete:");
+			for (int i=0; i<contacts.size(); i++) {
 			Contact contact= contacts.get(i);
 			System.out.println(i+1 +" - "+ contact.getContactN());
-			int input = (reader.nextInt());
-			contacts.remove(input-1);
-			System.out.println("Contact successfully removed.");
-			anotherWelcomeMsg();
+			}
+			try {
+				contacts.remove(input-1);
+				System.out.println("Contact successfully removed.");
+				anotherWelcomeMsg();
+			}
+			catch (NumberFormatException e) {
+			System.out.println(input+" isn't a valid option. Please enter a valid number ...");
 		}
 	}
+
 	
 	public void testWish() {
 		Wish w = new Wish("dashagh", 20);
