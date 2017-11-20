70f91c87c60a149aa9d65be7eb222ac67e1e2358
Unknown <peiman.shokouhian@gmail.com>
2017-11-20 14:08:58 +0100

    in progress

--- src/main/Event.java
+++ src/main/Event.java
@@ -2,15 +2,23 @@ package main;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.awt.Toolkit;
 
 
 public class Event implements Serializable {
 	private String description;
 	private Date date;
 	private String address;
+	private Date currentdate;
+	private Toolkit toolkit;
+	private Timer timer;
 
 	public Event (String description, Date date, String address) {
 		setDetails(description, date, address);
+		toolkit = Toolkit.getDefaultToolkit();
+		timer = new Timer();
 	}
 
 	public void setDetails(String description, Date date, String address) {
@@ -61,4 +69,12 @@ public class Event implements Serializable {
 	public void changeAddress(String newaddress) {
 		this.address = newaddress;
 	}
+	
+	public long getDateDifference(Date date) {
+		this.date = date;
+		long difference = (date.getTime()- currentdate.getTime())/1000;
+		return difference;
+	}
+	
+	//public getReminder
 }
